fed7c20e432525e1ca9a1472fd33a8f1314828b5
Derek Jacobi <derekjacobi@gmail.com>
2015-09-01 18:09:55 -0400

    search displays data

--- scripts/ang.js
+++ scripts/ang.js
@@ -1,5 +1,19 @@
-var movies = angular.module('IMDB', []);
+console.log("Angular Loaded");
 
-movies.controller('MovieController', function($scope, $http){
+var app = angular.module('omdb', []);
+
+app.controller('MovieController', function($scope, $http){
+  this.movieSearch = '';
+  currentThis = this;
+  this.formAction = function() {
+    $http.get('http://www.omdbapi.com/?t=' + this.movieSearch + '&y=&plot=short&r=json').
+      then(function(response){
+        currentThis.movieData = response.data;
+        console.log(currentThis.movieData);
+        $scope.movieTitle = currentThis.movieData.Title
+        $scope.plot = currentThis.movieData.Plot
+        $scope.actors = currentThis.movieData.Actors
+    })
+  }
 
 })
--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,7 +1,19 @@
 $( document ).ready(function() {
-  console.log("ready");
-    $('#submit').click(function(){
-      $('.search').addClass("searchUp")
-      $('.movie-title').addClass("searchSize")
-    })
+  console.log("JS Loaded");
+
+  var onSearch = function(){
+    $('.search').addClass("searchUp")
+    $('.movie-title').addClass("searchSize")
+  }
+
+  $('#submit').click(function(){
+    onSearch();
+  })
+
+  $(".movie-title").keyup(function(ev) {
+   if (ev.which === 13 ) {
+     onSearch()
+    }
+   })
+
 });
