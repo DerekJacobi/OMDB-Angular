58afe885b861be97f668211b4caae50184e54f8d
Derek Jacobi <derekjacobi@gmail.com>
2015-09-01 21:38:49 -0400

    searching for title instead of one movie

--- scripts/ang.js
+++ scripts/ang.js
@@ -6,13 +6,16 @@ app.controller('MovieController', function($scope, $http){
   this.movieSearch = '';
   currentThis = this;
   this.formAction = function() {
-    $http.get('http://www.omdbapi.com/?t=' + this.movieSearch + '&y=&plot=short&r=json').
+    // $http.get('http://www.omdbapi.com/?t=' + this.movieSearch + '&y=&plot=short&r=json').
+    $http.get('http://www.omdbapi.com/?s=' + this.movieSearch + '&y=&r=json').
       then(function(response){
-        currentThis.movieData = response.data;
-        console.log(currentThis.movieData);
-        $scope.movieTitle = currentThis.movieData.Title
-        $scope.plot = currentThis.movieData.Plot
-        $scope.actors = currentThis.movieData.Actors
+        console.log(response.data.Search);
+        $scope.movies = response.data.Search
+        // currentThis.movieData = response.data;
+        // console.log(currentThis.movieData);
+        // $scope.movieTitle = currentThis.movieData.Title
+        // $scope.plot = currentThis.movieData.Plot
+        // $scope.actors = currentThis.movieData.Actors
     })
   }
 
