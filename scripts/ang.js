42d4d9a0252771f63e314b1c3caaed39a361f68c
Derek Jacobi <derekjacobi@gmail.com>
2015-09-02 14:15:45 -0400

    selected movie info displays

--- scripts/ang.js
+++ scripts/ang.js
@@ -6,17 +6,17 @@ app.controller('MovieController', function($scope, $http){
   this.movieSearch = '';
   currentThis = this;
   this.formAction = function() {
-    // $http.get('http://www.omdbapi.com/?t=' + this.movieSearch + '&y=&plot=short&r=json').
     $http.get('https://www.omdbapi.com/?s=' + this.movieSearch + '&y=&r=json').
       then(function(response){
-        console.log(response.data.Search);
         $scope.movies = response.data.Search
-        // currentThis.movieData = response.data;
-        // console.log(currentThis.movieData);
-        // $scope.movieTitle = currentThis.movieData.Title
-        // $scope.plot = currentThis.movieData.Plot
-        // $scope.actors = currentThis.movieData.Actors
-    })
-  }
+    });
+  };
+  this.movieSelect = function(imdbID){
+    $http.get('https://www.omdbapi.com/?i=' + imdbID + '&y=&plot=short&r=json').
+      then(function(data){
+        console.log(data);
+        $scope.movie = data;
+      });
+  };
 
 })
