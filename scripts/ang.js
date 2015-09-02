e455ce2d08d9b9cd18cb8e479e3d0c0cf6aaf0bd
Derek Jacobi <derekjacobi@gmail.com>
2015-09-02 11:28:44 -0400

    https

--- scripts/ang.js
+++ scripts/ang.js
@@ -7,7 +7,7 @@ app.controller('MovieController', function($scope, $http){
   currentThis = this;
   this.formAction = function() {
     // $http.get('http://www.omdbapi.com/?t=' + this.movieSearch + '&y=&plot=short&r=json').
-    $http.get('http://www.omdbapi.com/?s=' + this.movieSearch + '&y=&r=json').
+    $http.get('https://www.omdbapi.com/?s=' + this.movieSearch + '&y=&r=json').
       then(function(response){
         console.log(response.data.Search);
         $scope.movies = response.data.Search
